a1e629b26bf45f008d27ca4ae1a794de65581e2c
Zakaria Fatahi <zakaria@buzzjective.com>
2021-12-26 08:25:54 +0100

    Add simplecov

--- Gemfile
+++ Gemfile
@@ -76,4 +76,5 @@ group :test do
   gem 'factory_bot_rails'
   gem 'faker'
   gem 'database_cleaner-active_record'
+  gem 'simplecov', require: false
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,12 @@
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+require 'simplecov'
+
+SimpleCov.start 'rails' do
+  add_group 'Services', 'app/services'
+  add_group 'Policies', 'app/policies'
+end
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest

3d6bd4194c3a992c838093bb8c8c7332784cffba
Zakaria Fatahi <zakaria@buzzjective.com>
2021-12-26 11:22:54 +0100

    Adjust database, redis and cable configs

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails/all"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -18,5 +18,20 @@ module BaseApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # Set Redis as the back-end for the cache.
+    config.cache_store = :redis_cache_store, {
+      url: ENV.fetch('REDIS_URL') { 'redis://redis:6379/1' },
+      namespace: 'cache'
+    }
+
+    # Mount Action Cable outside the main process or domain.
+    config.action_cable.mount_path = nil
+    config.action_cable.url = ENV.fetch('ACTION_CABLE_FRONTEND_URL') { 'ws://localhost:28080' }
+
+    # Only allow connections to Action Cable from these domains.
+    origins = ENV.fetch('ACTION_CABLE_ALLOWED_REQUEST_ORIGINS') { "http:\/\/localhost*" }.split(',')
+    origins.map! { |url| /#{url}/ }
+    config.action_cable.allowed_request_origins = origins
   end
 end

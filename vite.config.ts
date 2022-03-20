b1fa135e11d882170498e9ac3c76cd8931d15aba
Zakaria Fatahi <zakaria@buzzjective.com>
2022-03-20 22:53:38 +0100

    Define @ as an alias to the js root directory

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
+import path from "path"
 
 export default defineConfig({
+  resolve: {
+    alias: [
+      {
+        find: "@",
+        replacement: path.resolve(__dirname, "app", "javascript"),
+      },
+    ],
+  },
   plugins: [
     RubyPlugin(),
   ],

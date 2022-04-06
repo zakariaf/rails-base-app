5d1f162c011ba98f8e4a43a85db690447b60fd94
Zakaria Fatahi <zakaria@buzzjective.com>
2022-04-06 11:42:04 +0200

    Add a simple login and sign view

--- app/javascript/views/website/LoginPage.vue
+++ app/javascript/views/website/LoginPage.vue
@@ -1,15 +1,55 @@
 <template>
-  <h2 class="text-center">You are visiting the {{ pageName }} page!</h2>
+  <div class="form">
+    <div class="form-box pt-25">
+      <div class="form-bg py-30 px-50">
+        <span class="header pb-25">Sign in to your account</span>
+
+        <span v-if="showMessage" class="header pb-25"> You've clicked on Submit button </span>
+
+        <form @submit.prevent="submit">
+          <div class="field pb-25">
+            <label for="email">Email</label>
+            <input v-model="user.email" type="email" />
+          </div>
+
+          <div class="field pb-25">
+            <label for="password">Password</label>
+            <input v-model="user.password" type="password" />
+          </div>
+
+          <div class="field pb-25">
+            <input class="submit" type="submit" name="submit" value="Continue" />
+          </div>
+        </form>
+
+        <div class="footer pt-25 text-center">
+          <span>
+            Don't have an account? <router-link :to="{ name: 'signup' }">Sign up</router-link>
+          </span>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
+  name: 'LoginPage',
   data() {
     return {
-      pageName: 'Login',
+      user: {
+        email: '',
+        password: '',
+      },
+      showMessage: false,
     };
+  },
+  methods: {
+    submit() {
+      this.showMessage = true;
+    },
   },
 };
 </script>
 
-<style></style>
+<style lang="scss" scoped></style>

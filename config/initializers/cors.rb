de17156e2414584ed51ce0563eebf86753416453
Zakaria Fatahi <zakaria@buzzjective.com>
2022-04-17 13:02:21 +0200

    Add cors configs to allow requests from any origin on any resource

---
+++ config/initializers/cors.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins '*'
+    resource '*',
+             headers: :any,
+             methods: %i[get post put patch delete options head]
+  end
+end

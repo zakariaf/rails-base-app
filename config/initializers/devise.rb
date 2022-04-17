4017c4543613537213bec9d1bde4e4e6c5ca53b5
Zakaria Fatahi <zakaria@buzzjective.com>
2022-04-17 16:57:18 +0200

    Define JWT secret key

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -308,4 +308,9 @@ Devise.setup do |config|
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
+
+  # JWT Devise
+  config.jwt do |jwt|
+    jwt.secret = ENV.fetch('DEVISE_JWT_SECRET_KEY')
+  end
 end

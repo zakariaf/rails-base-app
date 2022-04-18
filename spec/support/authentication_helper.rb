310238d3ee61622b4dfa829ce34ca44cd2ffed37
Zakaria Fatahi <zakaria@buzzjective.com>
2022-04-18 13:11:54 +0200

    Define AuthenticationHelper

---
+++ spec/support/authentication_helper.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module AuthenticationHelper
+  def login(user)
+    post '/login', params: {
+      user: {
+        email: user.email,
+        password: user.password
+      }
+    }
+
+    @loggedin_user_token = response.headers['Authorization']
+  end
+
+  def loggedin_user_token
+    @loggedin_user_token
+  end
+
+  def auth_header
+    {
+      Authorization: loggedin_user_token
+    }
+  end
+end
+
+RSpec.configure do |config|
+  config.include AuthenticationHelper
+end

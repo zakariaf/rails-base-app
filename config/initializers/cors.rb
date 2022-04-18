cab57fd2033a3d3079a7d0112395dddf7ea408c8
Zakaria Fatahi <zakaria@buzzjective.com>
2022-04-18 11:18:53 +0200

    expose Authorization header which will be used to dispatch and receive JWT tokens in Auth headers

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,6 +5,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins '*'
     resource '*',
              headers: :any,
+             expose: ['Authorization'],
              methods: %i[get post put patch delete options head]
   end
 end

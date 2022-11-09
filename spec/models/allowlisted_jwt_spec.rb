32fde61fdb1ac25616e5dbd179792ec94815f859
Zakaria Fatahi <zakaria@buzzjective.com>
2022-11-09 23:37:31 +0100

    Fix rubocop errors by removingredundant spec type

--- spec/models/allowlisted_jwt_spec.rb
+++ spec/models/allowlisted_jwt_spec.rb
@@ -19,6 +19,6 @@
 #
 require 'rails_helper'
 
-RSpec.describe AllowlistedJwt, type: :model do
+RSpec.describe AllowlistedJwt do
   pending "add some examples to (or delete) #{__FILE__}"
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -24,6 +24,6 @@
 #
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
+RSpec.describe User do
   pending "add some examples to (or delete) #{__FILE__}"
 end

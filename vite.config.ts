0172aea84331bd1f521612af6e3838d42621a222
Zakaria Fatahi <zakaria@buzzjective.com>
2023-04-08 06:47:23 +0200

    Import aliases are defined out of the box by vite-plugin-ruby. No need to define @

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,8 @@ import { defineConfig } from 'vite';
 import RubyPlugin from 'vite-plugin-ruby';
 import vue from '@vitejs/plugin-vue';
 
-import path from 'path';
-
 export default defineConfig({
   resolve: {
-    alias: [
-      {
-        find: '@',
-        replacement: path.resolve(__dirname, 'app', 'javascript'),
-      },
-    ],
     extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
   },
   plugins: [vue(), RubyPlugin()],

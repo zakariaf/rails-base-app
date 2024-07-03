50f9390cd4ff323acd66941497f78b019a8b1c6a
Jim Huang <hjijin.gz@gmail.com>
2024-07-03 16:26:45 +0800

    update to rails 7.1.

--- config/application.rb
+++ config/application.rb
@@ -1,16 +1,32 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
-require 'sidekiq/web'
+require "rails"
+# Pick the frameworks you want:
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "active_storage/engine"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_mailbox/engine"
+require "action_text/engine"
+require "action_view/railtie"
+require "action_cable/engine"
+# require "rails/test_unit/railtie"
+require "sidekiq/web"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
 module BaseApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
+    config.load_defaults 7.1
+
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w(assets tasks))
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -19,6 +35,9 @@ module BaseApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # Don't generate system test files.
+    config.generators.system_tests = nil
 
     # Log to STDOUT. You could then collect logs using journald, syslog or forward them somewhere else.
     logger           = ActiveSupport::Logger.new(STDOUT)
@@ -48,7 +67,5 @@ module BaseApp
       ActiveSupport::SecurityUtils.secure_compare(username, ENV.fetch('SIDEKIQ_WEB_USERNAME', 'sidekiq-web-dashboard')) &&
         ActiveSupport::SecurityUtils.secure_compare(password, ENV.fetch('SIDEKIQ_WEB_PASSWORD', 'sidekiq-web-123'))
     end
-
-    config.action_mailer.default_url_options = { host: ENV['DEFAULT_HOST'] }
   end
 end
